d9156bfc41354f9b397927bc491d74bdb75d2928
Minh Cuong Tran <cuong.tran@svw.info>
2023-06-07 11:46:18 +0200

    setup running environment on test.svw.info

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $SCRIPT_DIR
+
+./main -yourMySQLdatabasepassword "Usm@1?/#Qv^avF" >> main.log 2>&1
